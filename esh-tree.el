bf1f3fd70f0f875806c1b238dd7b831d5e030ace
Clément Pit--Claudel <clement.pitclaudel@live.com>
2017-01-22 09:54:01 -0500

    trees: Finish prototype of segmenting logic

--- esh-tree.el
+++ esh-tree.el
@@ -48,7 +48,9 @@ Returns linked list including new link."
 
 (defun esh-tree--dll-pop (dll)
   "Remove first element of linked list DLL.
-Returns updated list."
+Returns updated list.  Beware of the fact that if DLL is the
+first link of a linked list, the caller must update it using
+something like (setq dll (esh-tree--dll-pop dll))."
   (pcase dll
     (`nil (error "Popping from empty list"))
     (`(,_ ,prev ,next)
@@ -101,10 +103,10 @@ END is exclusive."
         (push (nconc (list start end) (list merged-alist)) acc)))
     (nreverse acc)))
 
-(defun esh-tree--compute-tags-from-ranges (ranges props)
+(defun esh-tree--tags-from-annotated-ranges (ranges props)
   "Enumerate opening and closing tags from annotated RANGES.
 Each returned element has one of the following forms:
-  (\\='text START END)
+  (\\='text START END BUFFER)
   (\\='open POSITION (PROPERTY . VALUE))
   (\\='close POSITION (PROPERTY . VALUE))
 Each PROPERTY is one of PROPS.  There can be multiple consecutive (text …) nodes
@@ -123,12 +125,167 @@ if two consecutive ranges agree on all PROPS."
                 (push `(close ,start ,old) close-tags))
               (when new
                 (push `(open ,start ,new) open-tags)))))
-        (let ((text-node `((text ,start ,end))))
+        (let ((text-node `((text ,start ,end ,(current-buffer)))))
+          ;; `nreverse' ensures that closers are enumerated in reverse order
+          ;; from openers
           (setq tags (nconc text-node (nreverse open-tags) close-tags tags))))
       (setq prev-end end)
       (setq prev-alist alist))
     (dolist (pair prev-alist)
       (push `(close ,prev-end ,pair) tags))
     (nreverse tags)))
+
+(defun esh-tree--tag-property (tag)
+  "Read property changed by TAG."
+  (car (nth 2 tag)))
+
+(defun esh-tree--find-conflicts-1 (tag dll conflicts)
+  "Find conflicts for closer TAG in DLL.
+Corresponding opener is removed.  Conflicts are added to
+CONFLICTS.  Returns (DLL . CONFLICTS)."
+  (let ((pointer dll)
+        (prop (esh-tree--tag-property tag)))
+    (catch 'found
+      (while t
+        (let* ((prev-tag (esh-tree--dll-val pointer))
+               (prev-kind (car prev-tag)))
+          (cl-assert (eq prev-kind 'open) t "Unexpected: %S" prev-tag)
+          (if (eq (esh-tree--tag-property prev-tag) prop)
+              (throw 'found nil)
+            (push (cons prev-tag tag) conflicts)))
+        (setq pointer (esh-tree--dll-next pointer))))
+    (if (eq pointer dll)
+        ;; Opener was just before closer
+        (setq dll (esh-tree--dll-pop pointer))
+      (esh-tree--dll-pop pointer))
+    (cons dll conflicts)))
+
+(defun esh-tree--find-conflicts (tags)
+  "Find conflicting spans in TAGS.
+Two tags conflict if the overlap, but neither is included in the
+other.  Returns results as a list of (OPENER-1 . CLOSER-2)."
+  (let ((dll nil)
+        (conflicts nil))
+    (dolist (tag tags)
+      (pcase (car tag)
+        (`text)
+        (`open (esh-tree--dll-push-front tag dll))
+        (`close
+         (let ((res (esh-tree--find-conflicts-1 tag dll conflicts)))
+           (setq dll (car res) conflicts (cdr res))))))
+    conflicts))
+
+(defconst esh--latex-priorities
+  `(;; Fine to split:
+    ,@'(line-height newline :underline :foreground :weight :height :background)
+    ;; Should not split
+    ,@'(:slant :box display invisible))
+  "Priority order for text properties in LaTeX export.
+See `esh-tree--resolve-conflicts'.")
+
+(defun esh-tree--build-priorities (priorities)
+  "Create a hashmap mapping keys of PRIORITIES to their index in the list."
+  (let ((idx 0)
+        (pri (make-hash-table :test #'eq)))
+    (dolist (symb priorities)
+      (puthash symb (setq idx (1+ idx)) pri))
+    pri))
+
+(defvar esh-tree--priorities nil
+  "Internal variable used to pass priority maps around.")
+
+(defun esh-tree--tag-cmp (t1 t2)
+  "Compare two tags T1 and T2."
+  (let ((pos1 (nth 1 t1)) (pos2 (nth 1 t2)))
+    (cond
+     ((< pos1 pos2) -1)
+     ((> pos1 pos2) 1)
+     (t (let* ((types '(close open text))
+               (type1 (car t1)) (type2 (car t2))
+               (tp1 (cl-position type1 types))
+               (tp2 (cl-position type2 types)))
+          (cond
+           ((< tp1 tp2) -1)
+           ((> tp1 tp2) 1)
+           (t (cl-assert (not (eq type1 'text)))
+              (let* ((pri1 (gethash (esh-tree--tag-property t1) esh-tree--priorities))
+                     (pri2 (gethash (esh-tree--tag-property t2) esh-tree--priorities))
+                     (highest-priority-first (eq type1 'open)))
+                (cond
+                 ((< pri1 pri2) (if highest-priority-first 1 -1))
+                 ((> pri1 pri2) (if highest-priority-first -1 1))
+                 (t 0))))))))))
+
+(defun esh-tree--tag-less-p (t1 t2)
+  "Compare two tags T1 and T2 for sorting."
+  (< (esh-tree--tag-cmp t1 t2) 0))
+
+(defun esh-tree--compute-tags-to-resolve-conflicts (conflicts)
+  "Compute a list of additional openers and closers to resolve CONFLICTS.
+`esh-tree--priorities' is used to resolve conflicts, i.e. to determine which
+span to split when two spans intersect without nesting.  Earlier
+properties are split first."
+  (let ((more-tags nil))
+    (pcase-dolist (`((open ,op-pos ,op-pr) . (close ,cl-pos ,cl-pr)) conflicts)
+      (let* ((op-pri (gethash (car op-pr) esh-tree--priorities))
+             (cl-pri (gethash (car cl-pr) esh-tree--priorities)))
+        (unless op-pri (error "Unknown property %S" (car op-pr)))
+        (unless cl-pri (error "Unknown property %S" (car cl-pr)))
+        (let ((tl (if (<= op-pri cl-pri) `(,cl-pos ,op-pr) `(,op-pos ,cl-pr))))
+          (push (cons 'close tl) more-tags)
+          (push (cons 'open tl) more-tags))))
+    (sort more-tags #'esh-tree--tag-less-p)))
+
+(defun esh-tree--merge-sorted-tags (ts1 ts2)
+  "Merge two sorted lists of tags TS1 and TS2."
+  (let ((acc nil))
+    (while (or ts1 ts2)
+      (cond
+       ((and ts1 ts2)
+        (if (esh-tree--tag-less-p (car ts1) (car ts2))
+            (push (pop ts1) acc)
+          (push (pop ts2) acc)))
+       (ts1 (push (pop ts1) acc))
+       (ts2 (push (pop ts2) acc))))
+    (nreverse acc)))
+
+(defun esh-tree--resolve-conflicts (tags properties)
+  "Enrich and return TAGS to remove overlapping spans.
+See `esh-tree--compute-tags-to-resolve-conflicts' for info on
+PROPERTIES."
+  (let* ((conflicts (esh-tree--find-conflicts tags))
+         (esh-tree--priorities (esh-tree--build-priorities properties))
+         (extras (esh-tree--compute-tags-to-resolve-conflicts conflicts)))
+    (esh-tree--merge-sorted-tags tags extras)))
+
+(defun esh-tree--buffer-to-tags-list (text-props face-attrs priority-ranking)
+  "Construct a properly nested list of tags from current buffer.
+TEXT-PROPS and FACE-ATTRS specify which properties to keep track
+of.  PRIORITY-RANKING ranks all these properties in order of
+tolerance to splitting (if a property comes late in this list,
+ESH will try to preserve this property's spans when resolving
+conflicts."
+  (let* ((ranges (esh--buffer-ranges))
+         (annotated-ranges (esh-tree--annotate-ranges ranges text-props face-attrs))
+         (tags (esh-tree--tags-from-annotated-ranges annotated-ranges priority-ranking))
+         (unconflicted-tags (esh-tree--resolve-conflicts tags priority-ranking)))
+    unconflicted-tags))
+
+(defun esh-tree--export (exporter tags)
+  "Call EXPORTER on `car' of list of TAGS until it returns nil.
+On each iteration, the return value of EXPORTER is used as the next STREAM."
+  (while tags
+    (setq tags (funcall exporter tags))))
+
+(defun esh-tree--drop-until-close (prop stream)
+  "Drop elements of STREAM until a closer for PROP is found."
+  (let ((depth 1))
+    (while (> depth 0)
+      (pcase (car (pop stream))
+        (`nil (error "No closing tag found for %S" prop))
+        (`open (cl-incf depth))
+        (`close (cl-decf depth)))))
+  stream)
+
 (provide 'esh-tree)
 ;;; esh-tree.el ends here
