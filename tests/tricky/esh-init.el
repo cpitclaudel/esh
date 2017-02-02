b53110c0dcfde5ba0b6b350d3d3f369bbd197509
Clément Pit--Claudel <clement.pitclaudel@live.com>
2017-02-02 18:21:08 -0500

    tests: Include a tricky (and currently incorrect) example of overlap
    There are two issues with this test: spurious blanks, and sub-optimal nesting.
    The latter is likely to require a change of algorithm.
    
    Here's a short description of the problem. Suppose there's one interval
    a:5-15. Insert b:1-10, which splits a into a1:5-10 and a2:10-15.  Then insert
    c:1-8, which splits a1 into a11:5-8 and a12:8-10, and b into b1:1-8 and b2:8-10.
    But now b2 fits into a12, and a11 fits in b1.
    
    The trick is that inserting b1 and b2 into a1':5-8 a2':8-15 would not have
    required further splits.  In other words, cutting a to accommodate b wasn't
    right, since b was going to be cut later on, rendering the original cut useless.

--- tests/tricky/esh-init.el
+++ tests/tricky/esh-init.el
@@ -1,8 +1,8 @@
-(when esh-server-initializing
+(when (bound-and-true-p esh-server-initializing)
   (load-theme 'tango t))
 
 (define-minor-mode tricky-mode
-  "Major mode demonstrating tricky highlighting."
+  "Minor mode demonstrating tricky highlighting."
   :lighter " trk"
   (add-to-list 'font-lock-extra-managed-props 'line-height)
   (esh-add-keywords
@@ -15,9 +15,30 @@
 
 
 (define-minor-mode subsup-mode
-  "Major mode demonstrating tricky highlighting."
+  "Minor mode demonstrating subscripts and superscripts."
   :lighter " subssup"
   (add-to-list 'font-lock-extra-managed-props 'display)
   (esh-add-keywords
     `(("x\\(.*\\)" (1 '(face nil display (raise 1))))
       ("y\\(.*\\)" (1 '(face nil display (raise -1)))))))
+
+(defun ~/setup-overlays ()
+  "Add a collection of conflicting overlays to the current buffer."
+  (erase-buffer)
+  (insert "abcdefghijklmno")
+  (put 'esh-test 'face '((:box "#000000")))
+  (mapc #'delete-overlay (overlays-in (point-min) (point-max)))
+  (let ((ov (make-overlay 1 9)))
+    (overlay-put ov 'face '((:foreground "#FF0000"))))
+  (let ((ov (make-overlay 3 11)))
+    (overlay-put ov 'face '((:background "#00FF00"))))
+  (let ((ov (make-overlay 5 14)))
+    (overlay-put ov 'face '((:weight bold :slant italic))))
+  (let ((ov (make-overlay 7 16)))
+    (overlay-put ov 'category 'esh-test)))
+
+(define-minor-mode overlays-mode
+  "Minor mode demonstrating overlays over text."
+  :lighter " overlays"
+  (~/setup-overlays)
+  (add-hook 'esh-pre-highlight-hook #'~/setup-overlays nil t))
